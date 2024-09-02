0042aab820d605db81872e5282a97fb3d9957d5c
Felipe de oliveira <106187032+felipedeoliveiraneves@users.noreply.github.com>
2024-09-02 19:04:23 -0300

    Introdução Python

---
+++ aula4.py
@@ -0,0 +1,24 @@
+# Tipo int e float
+# int -> Numero inteiro
+# O tipo int representa qualquer numero
+# Positivo ou negativo. int sem sinal é considerado 
+# Positivo.
+print(10)
+print(-11)
+print(0)
+
+# float -> Numero com ponto flutuante 
+# O tipo float representa qualquer numero 
+# Positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+
+print(1.1)
+print(2.3)
+print(0.0, -1.9)
+
+# Afunção type mostra o tipo que o python
+# inferiu ao valor
+print(type("Felipe"))
+print(type(2511))
+print(type(0.0), type(-1.0), type(5.0))
+
---
+++ aula5.py
@@ -0,0 +1,14 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# Só existe duas resposta possivel:
+# Sim (true) ou Não (false).
+# Existem varios operadores para "questionar".
+# Dentro deles o ==, que é um operador logico que 
+# questiona  se um valor e igual a outro 
+
+print(10 == 10) #Sim => True (Verdadeiro)
+print(10 == 12) #Não => False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 12))
