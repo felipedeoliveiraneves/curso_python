fd942e99023967fd2d24eb312e9a1c4b3e942608
Felipe de oliveira <106187032+felipedeoliveiraneves@users.noreply.github.com>
2024-09-04 19:26:07 -0300

    Estudo Python

---
+++ aula7.py
@@ -0,0 +1,18 @@
+# Variaveis e usada para salvar alguma coisa na memoria do computador 
+# PEP8:inicie vareaveis com letras minusculas, pode usar 
+# numeros e underline_.
+# O sinal de = é operador de atribuição. ele e usado para 
+# atribuir um valor a um nome (Variavel)
+# Uso: nome_variavel = expressão 
+
+nome_completo = "Felipe de Oliveira"
+soma_dois_mais_dois = 2+2
+int_um = bool("1")
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+nome = "Felipe"
+idade = 30
+maior_de_idade = idade >= 18
+print("Nome:", nome, "Idade:", idade)
+print("É maior?", maior_de_idade)
